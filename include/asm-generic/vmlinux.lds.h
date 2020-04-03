c9108f5db19c350cd06c34599a701f4cd7710e37
Danny Lin <danny@kdrag0n.dev>
2020-04-03 11:50:04 -0700

    vmlinux.lds.h: Coalesce LLVM dead code elimination sections
    A recent LLVM commit [1] made LLD stop coalescing these temporary
    LLVM-generated sections by default to match GNU ld behavior. However,
    the kernel can fail to boot if these sections remain separate in the
    image, and the overhead from retaining so many small sections is
    significant.
    
    Coalesce these specific sections to fix the issue rather than coalescing
    all .data..* sections, since some are created intentionally by Linux and
    should not be combined.
    
    Note that these sections are only generated when dead code elimination
    is enabled.
    
    [1] https://github.com/llvm/llvm-project/commit/9e33c096476ab5e02ab1c8442cc3cb4e32e29f17
    
    Suggested-by: Fangrui Song <maskray@google.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Farizmaulana

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -65,11 +65,11 @@
  * .data. We don't want to pull in .data..other sections, which Linux
  * has defined. Same for text and bss.
  */
-#ifdef CONFIG_LD_DEAD_CODE_DATA_ELIMINATION
+#if defined(CONFIG_LD_DEAD_CODE_DATA_ELIMINATION) || defined(CONFIG_LTO_CLANG)
 #define TEXT_MAIN .text .text.[0-9a-zA-Z_]*
 #define TEXT_CFI_MAIN .text.cfi .text.[0-9a-zA-Z_]*.cfi
-#define DATA_MAIN .data .data.[0-9a-zA-Z_]* .data..compoundliteral* .data..L_MergedGlobals*
-#define BSS_MAIN .bss .bss.[0-9a-zA-Z_]* .bss..compoundliteral* .bss..L_MergedGlobals*
+#define DATA_MAIN .data .data.[0-9a-zA-Z_]* .data..compoundliteral* .data..L* .data..L_MergedGlobals*
+#define BSS_MAIN .bss .bss.[0-9a-zA-Z_]* .bss..compoundliteral* .bss..L* .bss..L_MergedGlobals*
 #else
 #define TEXT_MAIN .text
 #define TEXT_CFI_MAIN .text.cfi
@@ -469,7 +469,6 @@
 		*(.text..ftrace)					\
 		*(TEXT_CFI_MAIN) 					\
 		*(.ref.text)						\
-		*(.text.asan.* .text.tsan.*)				\
 	MEM_KEEP(init.text)						\
 	MEM_KEEP(exit.text)						\
 
